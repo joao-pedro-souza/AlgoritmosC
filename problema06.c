b183e988d296e7b9aef28d4a43b3f6942c979961
João Pedro Souza <pcjoaopedro40@gmail.com>
2022-09-29 10:31:49 -0300

    Update problema06.c

--- problema06.c
+++ problema06.c
@@ -6,11 +6,11 @@ int main(void)
     int n;
 
     printf("Digite um numero: ");
-    scanf("%i", &n);
+    scanf("%d", &n);
 
     if (n%5==0)
-        printf("%i e divisivel por 5.", n);
+        printf("%d e divisivel por 5.", n);
     else   
-        printf("%i nao e divisivel por 5.", n);
+        printf("%d nao e divisivel por 5.", n);
     return (0);
 }
