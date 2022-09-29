1fd2d58dc0d7a0959d1e03e6857504f55c23caa9
João Pedro Souza <pcjoaopedro40@gmail.com>
2022-09-29 10:30:57 -0300

    Update problema04.c

--- problema04.c
+++ problema04.c
@@ -8,11 +8,11 @@ int main(void)
     int n1, n2, n3;
 
     printf("Escreva o numero base: ");
-    scanf("%i", &n1);
+    scanf("%d", &n1);
     printf("Escreva o numero expoente: ");
-    scanf("%i", &n2);
+    scanf("%d", &n2);
     
     n3 = pow(n1,n2);
-    printf("A potencia e: %i", n3);
+    printf("A potencia e: %d", n3);
     return (0);
 }
