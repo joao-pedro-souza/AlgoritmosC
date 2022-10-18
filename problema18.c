87c852e9abec4d943ca94883114e53f5781fa3be
João Pedro Souza <pcjoaopedro40@gmail.com>
2022-10-18 17:27:17 -0300

    Update problema18.c

---
+++ problema18.c
@@ -0,0 +1,4 @@
+/* Em uma avaliação de um produto, o cliente responde sua opinião (1 – satisfatório; 2 – indiferente; 
+3 – insatisfatório). Faça um programa que leia idade e opinião e apresente: o número de clientes que responderam 
+satisfatório, a média de idade dos clientes que opinaram como satisfatório, e o número de clientes que responderam 
+insatisfatório. O programa se encerra quando for digitado o valor zero para idade. */
